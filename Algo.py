f211531a81f616ce7c6540f8913944dba692cf10
justinienghorra <justinienghorra@hotmail.fr>
2018-03-03 18:36:27 +0100

    Ajout des translations dans la vérification des états déjà parcourus

--- Algo.py
+++ Algo.py
@@ -1,20 +1,22 @@
 import Functions
+import time
 
 
 #*** Algorithme ***
+beginTime = time.time()
 listResults = [] # liste des résultats menant vers l'état tout mort
 listTemp = [] # liste temporaire
-listEtats = Functions.generateStates(5) # # liste de tous les états possibles
+length = 6
+listEtats = Functions.generateStates(length) # # liste de tous les états possibles
 listLoop = []# liste de tous les états bouclant à l'infini
-toutMort = [[0,0,0,0,0],[0,0,0,0,0],[0,0,0,0,0],[0,0,0,0,0]] # etat tout mort, correspond à une grille de case vide
+toutMort = [[0,0,0,0,0,0],[0,0,0,0,0,0],[0,0,0,0,0,0],[0,0,0,0,0,0]] # etat tout mort, correspond à une grille de case vide
 listResults.append(toutMort) #Ajouter l'etat tout mort à la liste des resultats
 
 stop = False
-
 while len(listEtats) != 0: #tant qu'on n'a pas verifié tous les états
     etatFutur = Functions.nextState(listEtats[0], toutMort) # Utilise la fonction nextState pour obtenir l'etat au temps t+1
 
-    if(etatFutur == toutMort): # On vérifie que le prochain etat n'est pas l'etat tout mort
+    if(Functions.alreadyVisited(etatFutur, listResults, length)): # On vérifie que le prochain etat n'est pas l'etat tout mort
       listResults.append(listEtats[0]) # si c'est le cas alors on ajoute l'état à la liste des resultats
       listEtats.remove(listEtats[0]) # Supprime l'element de la liste
 
@@ -26,15 +28,16 @@ while len(listEtats) != 0: #tant qu'on n'a pas verifié tous les états
         listTemp.append(listEtats[0]) # On ajoute l'etat de base dans une liste temporaire
 
         while(not stop): # Tant qu'on a pas trouve de resultat (boucle ou etat tout mort)
-            etatFutur = Functions.nextState(listTemp[-1], toutMort) # Recuppere le dernier element inseré dans la liste et regarde l'etat au temps t+1
+            etatFutur = Functions.nextState(etatFutur, toutMort) # Recuppere le dernier element inseré dans la liste et regarde l'etat au temps t+1
 
-            if (etatFutur == toutMort or etatFutur in listResults): # si on fini par arriver vers l'etat tout mort ou vers un etat y arrivant
+            if (Functions.alreadyVisited(etatFutur, listResults, length)): # si on fini par arriver vers l'etat tout mort ou vers un etat y arrivant
                 stop = True # On a trouvé un resultat plus besoin de chercher dans les successeurs
                 listResults.extend(listTemp) # On ajoute tous les elements de la liste à la liste contenant les resultats
                 #Supprime tous les etats de la liste temp des etats encore non parcouru de la liste listEtats
                 for item in listTemp: # Supprime tous les elemets qui ont été parcouru de la liste contenant tous les etats possibles
-                    if(item in listEtats):
-                        listEtats.remove(item)
+                    tupleTemp = Functions.alreadyVisitedWithOldState(item, listEtats, length)
+                    if(tupleTemp[0]):
+                        listEtats.remove(tupleTemp[1])
                 listTemp=[] # On réinitialise la liste
 
             elif(etatFutur in listTemp or etatFutur in listLoop): # On va boucler à l'infini
@@ -42,13 +45,16 @@ while len(listEtats) != 0: #tant qu'on n'a pas verifié tous les états
                 listLoop.extend(listTemp) # On ajoute à la liste des etats bouclant à l'infini la liste d'etats que l'on parcourait
                 #Supprime tous les etats de la liste temp des etats encore non parcouru de la liste listEtats
                 for item in listTemp: # Supprime tous les elemets qui ont été parcouru de la liste contenant tous les etats possibles
-                    if(item in listEtats):
-                        listEtats.remove(item)
+                    tupleTemp = Functions.alreadyVisitedWithOldState(item, listEtats, length)
+                    if(tupleTemp[0]):
+                        listEtats.remove(tupleTemp[1])
                 listTemp=[] # On réinitialise la liste
 
             else:
                 listTemp.append(etatFutur)
         stop = False # Reinitialise la variable
-
-for item in listResults:
-    print(item)
+print(len(listResults))
+endTime = time.time()
+print(endTime-beginTime)
+#for item in listResults:
+#    print(item)
--- Functions.py
+++ Functions.py
@@ -36,6 +36,49 @@ def nextState(a, allDead):
 def show(a):
     print('\n'.join([' '.join([str(cell) for cell in row]) for row in a]) + '\n')
 
+def alreadyVisited(newState, visitedList, length):
+    found = False
+    i=0
+    while not found and i<length: #on s'arrete si on a fait length decalages
+        item = newState.pop()
+        newState.insert(0, item) #la derniere ligne devient la premiere
+        #print("B : ", newState)
+        if newState in visitedList: #arret si cette nouvelle grille a deja ete ajoutee precedemment
+            found = True
+        i+=1
+    i=0
+    while not found and i<length: 
+        for line in newState: #le dernier element de chaque ligne devient le premier
+            item = line.pop() 
+            line.insert(0, item)
+        #print("B : ", newState)
+        if newState in visitedList: #arret si cette nouvelle grille a deja ete ajoutee precedemment
+            found = True
+        i+=1
+    return found
+
+def alreadyVisitedWithOldState(newState, visitedList, length):
+    found = False
+    i=0
+    while not found and i<length: #on s'arrete si on a fait length decalages
+        item = newState.pop()
+        newState.insert(0, item) #la derniere ligne devient la premiere
+        #print("B : ", newState)
+        if newState in visitedList: #arret si cette nouvelle grille a deja ete ajoutee precedemment
+            found = True
+        i+=1
+    i=0
+    while not found and i<length: 
+        for line in newState: #le dernier element de chaque ligne devient le premier
+            item = line.pop() 
+            line.insert(0, item)
+        #print("B : ", newState)
+        if newState in visitedList: #arret si cette nouvelle grille a deja ete ajoutee precedemment
+            found = True
+        i+=1
+    return found, newState
+
+
 def generateStates(length):
 	lst = list(map(list, itertools.product([0, 1], repeat=length)))
 	finalElements= []
@@ -44,32 +87,11 @@ def generateStates(length):
 	    for e2 in lst:
 	        for e3 in lst:
 	            for e4 in lst:
-	                found = False
-	                i=0
 	                newState = [e1,e2,e3,e4] #chaque e est une ligne de la grille
 	                #print("A : ")
 	                #print('\n'.join([' '.join([str(cell) for cell in row]) for row in newState]) + '\n')
-	                while not found and i<length: #on s'arrete si on a fait length decalages
-	                    item = newState.pop()
-	                    newState.insert(0, item) #la derniere ligne devient la premiere
-	                    #print("B : ", newState)
-	                    if newState in finalElements: #arret si cette nouvelle grille a deja ete ajoutee precedemment
-	                        found = True
-	                    i+=1
-	                i=0
-	                while not found and i<length: 
-	                    for line in newState: #le dernier element de chaque ligne devient le premier
-	                        item = line.pop() 
-	                        line.insert(0, item)
-	                    #print("B : ", newState)
-	                    if newState in finalElements: #arret si cette nouvelle grille a deja ete ajoutee precedemment
-	                        found = True
-	                    i+=1
-	                if not found: # si l'element n'a pas ete trouve precedemment, on l'ajoute aux etats possibles
-	                    #print("B : ")
-	                    #print('\n'.join([' '.join([str(cell) for cell in row]) for row in newState]) + '\n')
+	                if not alreadyVisited(newState, finalElements, length):
 	                    finalElements.append(newState)
-	print("Final : ",len(finalElements))
 	return finalElements
 
 
